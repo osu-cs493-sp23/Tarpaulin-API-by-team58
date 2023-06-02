722ba43624cfc5c41b66960f3b44635d27bb80f7
Xuelu Feng <gcctryg@gmail.com>
2023-06-02 01:36:47 -0700

    add readme and server
    still need mysql and docker setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const port = 8000
+
+app.use(function (req, res, next) {
+    console.log("== Request received")
+    console.log("   -- Method:", req.method)
+    console.log("   -- URL:", req.url)
+    next()
+})
+
+
+app.listen(port, function() {
+    console.log("== Server is running on port", port)
+})
