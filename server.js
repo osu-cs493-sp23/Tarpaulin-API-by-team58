cd862ae21db6bd18959f05a142d087c96baab9f7
Xuelu Feng <gcctryg@gmail.com>
2023-06-02 23:39:52 -0700

    500 and 404 status added

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ app.use(function (req, res, next) {
     next()
 })
 
+//This route will catch invalid URL and return
+//a response with a 404 status to client
+app.use('*', function (req, res, next) {
+    res.status(404).json({
+      error: `Requested URL does not exist: ${req.originalUrl}`
+    })
+})
+
+/*
+ * This route will catch any errors thrown from our API endpoints and return
+ * a response with a 500 status to the client.
+ */
+app.use('*', function (err, req, res, next) {
+    console.error("== Error:", err)
+    res.status(500).send({
+        err: "Server error.  Please try again later."
+    })
+})
+  
 
 app.listen(port, function() {
     console.log("== Server is running on port", port)
