bc9ed136e41675384cc6cbb3cd1746214c7b67b5
JarodLokrantz <lokrantj@oregonstate.edu>
2023-06-08 20:36:34 -0700

    added user sequelize definition and POST endpoint

--- api/users.js
+++ api/users.js
@@ -1,6 +1,34 @@
 const { Router } = require('express')
 const { ValidationError } = require('sequelize')
+const { User, userSchema, userClientFields } = require("../models/user.js")
+const { validateAgainstSchema } = require("../lib/dataValidation.js")
 
 const router = Router()
+
+router.post("/", async function (req, res, next){
+	var newUser = req.body
+	if (!validateAgainstSchema(newUser, userSchema)){
+		res.status(400).json({
+			error: "The request body was either not present or did not contain a valid User object containing required fields: \"name,\" \"email,\" \"password,\" and \"role.\""
+		})
+		return
+	}
+
+	var user = {}
+	try {
+		user = await User.create(newUser, userClientFields)
+	} catch (err){
+		if (err instanceof ValidationError){
+			res.status(400).json({
+				error: "A user already exists with that email."
+			})
+			return
+		}
+		next(err)
+		return
+	}
+
+	res.status(201).json({id: user.id})
+})
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,2 +1,15 @@
 const { DataTypes } = require("sequelize")
 const sequelize = require("../lib/sequelize.js")
+
+const userFields = {
+	name: {type: DataTypes.STRING, allowNull: false, unique: false},
+	email: {type: DataTypes.STRING, allowNull: false, unique: true},
+	password: {type: DataTypes.STRING, allowNull: false, unique: false},
+	role: {type: DataTypes.STRING, allowNull: false, unique: false}
+}
+
+const User = sequelize.define("user", userFields)
+
+exports.User = User
+exports.userSchema = userFields
+exports.userClientFields = Object.keys(userFields)
