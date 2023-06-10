e8ab418bbb37c05b66c9da93af0506d4f84d5736
SS-CC-YY <songchenyu2@gmail.com>
2023-06-09 22:04:35 -0700

    complete user create login and shown by id

--- api/users.js
+++ api/users.js
@@ -1,6 +1,94 @@
 const { Router } = require('express')
 const { ValidationError } = require('sequelize')
+const bcrypt = require("bcryptjs")
+
+
+const {Assignment} = require('../models/assignment')
+const {Course} = require('../models/course')
+const {User, UserClientFields} = require('../models/user')
+const {generateAuthToken, requireAuthentication, isAdmin} = require('../lib/hateoasHelpers')
+
 
 const router = Router()
+
+// create user
+router.post('/', async function (req, res, next){
+	if (!isAdmin(req) && req.body.admin){
+		res.status(403).json({
+			error: "Invalid role to create Admin"
+		})
+		return
+	}
+	try{
+		const user = await User.create(req.body,UserClientFields)
+		const hash = await bcrypt.hashSync(user.password, 8)
+		user.password = hash
+		console.log(" -- userToInsert:", user)
+
+		res.status(201).send({
+			id: user.id
+		})
+	} catch (e) {
+		if (e instanceof ValidationError) {
+			res.status(400).send({ error: e.message})
+		} else {
+			next(e)
+		}
+	}
+})
+
+// login the user
+router.post('/login', async function (req, res, next) {
+	if (req.body && req.body.id && req.body.password ) {
+		try {
+			authenticated = await User.findByPk(req.body.id)
+			authenticated = await bcrypt.compare(req.body.password, authenticated.password)
+		
+			if (authenticated) {
+			//find out this user
+			const userData = await User.findByPk(req.body.id)
+			const token = generateAuthToken(userData)
+			res.status(200).send({
+				token: token
+			})
+		}else{
+			res.status(401).send({
+				error: "invalid authentication credential"
+			})
+		}
+		} catch(e) {
+		next(e)
+		}
+	} else {
+		res.status(400).send({
+			err: "request body requires `id` and `password`."
+		})
+	}
+})
+
+// based on id to get all information about user
+router.get('/:id', requireAuthentication, async function (req, res, next) {
+	if (req.user === Number(req.params.id) || isAdmin(req)) {
+		try {
+			const user = await User.findByPk(req.params.id)
+			if (user) {
+				res.status(200).send({
+				username: user.name,
+				email: user.email,
+				role: user.role
+			})
+		} else {
+			next()
+		}
+		} catch (e) {
+			next(e)
+		}
+	} else {
+		res.status(403).send({
+			error: "Unauthorized to access the specific resource"
+		})
+	}
+})
+
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,2 +1,35 @@
 const { DataTypes } = require("sequelize")
 const sequelize = require("../lib/sequelize.js")
+
+const { Course } = require("./course.js")
+
+const bcrypt = require("bcryptjs")
+
+const User = sequelize.define('user',{
+	id: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true},
+	name: {type: DataTypes.STRING, allowNull: false},
+	email: {type: DataTypes.STRING, allowNull: false},
+	password: {
+		type: DataTypes.STRING,
+		set(value) {
+			// Storing passwords in plaintext in the database is terrible.
+			// Hashing the value with an appropriate cryptographic hash function is better.
+			this.setDataValue('password', bcrypt.hashSync(value));
+		},
+		allowNull: false
+	},
+	role: {type: DataTypes.STRING, allowNull: false},
+	admin: {type: DataTypes.BOOLEAN, allowNull: true, defaultValue: false},
+	// teacher: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
+	// student: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true}
+})
+
+exports.User = User
+
+exports.UserClientFields = [
+	'name',
+	'email',
+	'password',
+	'role', //student or teacher
+	'admin'
+]
